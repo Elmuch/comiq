8ba792a75d8d674bc8967e52c890c4eddfba7699
Jessie Samuelson <jessie.samuelson@gmail.com>
2016-05-07 17:46:46 -0400

    adds canvas into view/js

--- public/js/main.js
+++ public/js/main.js
@@ -24,3 +24,95 @@
 
 
 })(jQuery)
+
+
+// Canvas
+
+var video = document.getElementById('video-preview');
+var initialized = 0;
+
+video.oncanplay = function() {
+  if ( initialized ) return;
+  initialize();
+}
+
+var set_button = $('#set-style');
+set_button.click(setCanvas)
+
+var moments = {};
+
+function initialize () {
+  initialized = 1;
+
+  var still_count = 4;
+  var width       = 400;
+
+  for ( var i = 0; i < still_count; i++ ) {
+    var id = "still-" + (i + 1);
+    var el = document.getElementById(id);
+
+    var entry = new Still({
+      index: i + 1,
+      id:    id,
+    })
+
+    moments[id] = entry;
+    entry.setStill();
+  }
+  $("#still-1").addClass('selected');
+}
+
+function Still (opts) {
+  this.index = opts.index;
+  this.id    = opts.id;
+}
+
+Still.prototype = {
+  setStill: function() {
+    var output = document.getElementById('output');
+
+    var width  = 98;
+    var height = 54.25;
+    
+    var el       = document.createElement('div');
+    el.className = "still";
+    el.id        = "still-" + this.index;
+    
+    var canvas       = document.createElement('canvas');
+    canvas.className = "canvas";
+    
+    el.appendChild(canvas);
+    output.appendChild(el);
+
+    $(el).click(function(e){
+      moments[this.id].updateSelected();
+    });
+  },
+  updateSelected: function(){
+    $(".still").removeClass('selected');
+    $("#" + this.id).addClass('selected');
+  }
+}
+
+function setCanvas(){
+  var el = document.getElementsByClassName('selected')[0];
+
+  var fc = el.firstChild;
+
+  while( fc ) {
+      el.removeChild( fc );
+      fc = el.firstChild;
+  }
+
+  var width  = 98;
+  var height = 54.25;
+
+  var canvas       = document.createElement('canvas');
+  
+  el.appendChild(canvas);
+
+  var ctx       = canvas.getContext("2d");
+  canvas.width  = width;
+  canvas.height = height;
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+}
