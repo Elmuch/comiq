99e9d37d178fccacf7d125f12639e067cd6933d5
Elijah Gachuhi <elgachuhi@gmail.com>
2016-05-07 16:31:48 -0400

    write base64 bin to file

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var app = express();
 var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
+app.locals.baseDir = __dirname
 
 // view engine setup
 
--- routes/tag.js
+++ routes/tag.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 
 var Clarifai = require('../lib/clarifai_node.js');
 var router = express.Router();
@@ -8,9 +9,18 @@ var secret = "6vEWYHsbqkP88S3CFyTVcJSPnrif0M-mu_muaOlM";
 
 router.post('/', function(req, res) {
   Clarifai.initAPI(clientID, secret);
-  var img_url = req.body.img;
-  console.log(img_url)
-  var tags = Clarifai.tagURL("http://static1.gamespot.com/uploads/scale_large/104/1049837/2891179-batman-arkham_knight-review_nologo_20150618.jpg", null, function(err, tagRes){
+  var img_data = req.body.img;
+    img_data = img_data.replace(/^data:image\/\w+;base64,/, '');
+    img_dir = req.app.locals.baseDir + '/public/img/';
+
+  fs.writeFile(img_dir + 'tester.gif', img_data, {encoding: 'base64'}, function(err){
+    console.log("Done writing");
+  });
+
+  img_url = img_dir + 'test.jpg';
+  console.log(img_url);
+
+  var tags = Clarifai.tagURL(img_url, null, function(err, tagRes){
   if(tagRes){
     res.json({tags: tagRes.results});
   }else{
